9bf9714403118a8ba66ee33fb04e8f7088c496d1
Niktont <legobooster@yandex.ru>
2026-05-05 16:27:21 +0300

    разработка backend_server.py

--- server/backend_server.py
+++ server/backend_server.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, jsonify
 from sqlalchemy import select
 
 from databases import create_session
-from models import User
+from models import User, Post, UserORM, PostORM
 
 app = Flask(__name__)
 
@@ -11,11 +11,17 @@ def index():
     return 'backend-server is running'
 
 @app.route('/users', methods=['GET'])
-def get_users():
-    query = select(User).order_by(User.username)
+def get_users(): # list[User]
+    query = select(UserORM).order_by(UserORM.username)
     session = create_session()
-    result = session.execute(query).scalars()
-    return result
+    raw_users = session.execute(query).scalars()
+    users: list[User] = [User.from_custom_orm(user) for user in raw_users]
+    return jsonify(users)
 
 @app.route('/posts', methods=['GET'])
-def get_posts():
+def get_posts(): # list[Post]
+    query = select(PostORM).order_by(PostORM.timestamp)
+    session = create_session()
+    raw_posts = session.execute(query).scalars()
+    posts: list[Post] = [Post.from_custom_orm(post) for post in raw_posts]
+    return jsonify(posts)
