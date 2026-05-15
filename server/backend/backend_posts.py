5ea61f17f2e45acd5127c0005adb8ad22af4dca0
King-Of-End <legobooster17@gmail.com>
2026-05-15 17:30:35 +0300

    улучшил бэкенд

--- server/backend/backend_posts.py
+++ server/backend/backend_posts.py
@@ -9,36 +9,58 @@ from .backend_server import app
 
 
 def get_posts_by_query(query: Select) -> list[Post]:
+    """Получает посты по запросу Select"""
+    # создаем сессию базы данных
     with create_session() as session:
+        # выполняем запрос
         raw_posts = session.execute(query).scalars().all()
+
+        # преобразовываем в pydantic-модели
         posts: list[Post] = [Post.from_custom_orm(post).model_dump(mode='json') for post in raw_posts]
+
+    # возвращаем список постов
     return posts
 
 
 @app.route('/posts')
 def get_posts():  # list[Post]
+    """Получить все посты"""
+
+    # создаём запрос
     query = select(PostORM).order_by(PostORM.created_at.desc())
 
+    # выполняем запрос
     posts: list[Post] = get_posts_by_query(query)
 
+    # преобразовываем в json и возвращаем
     return jsonify(posts)
 
 
 @app.route('/posts/<int:post_id>')
 def get_post_by_id(post_id: int):
+    """Получить посты по id"""
+
+    # создаём запрос
     query = select(PostORM).where(PostORM.id == post_id).order_by(PostORM.created_at)
 
+    # выполняем запрос
     post: Post = get_posts_by_query(query)[0]
 
+    # преобразовываем в json и возвращаем
     return jsonify(post)
 
 
 @app.route('/posts/by/<string:author>', methods=['GET'])
 def get_posts_by_author(author: str):
+    """Получить посты по автору"""
+
+    # создаём запрос
     query = select(PostORM).where(PostORM.author_username == author).order_by(PostORM.created_at.desc())
 
+    # выполняем запрос
     posts: list[Post] = get_posts_by_query(query)
 
+    # преобразовываем в json и возвращаем
     return jsonify(posts)
 
 
@@ -78,21 +100,49 @@ def get_posts_by_author(author: str):
 
 @app.route('/posts', methods=['POST'])
 def add_post():
+    """
+    Функция для добавления поста
+    """
+
+    # получаем данные из запроса
     data = request.get_json()
+
+    # создаем сессию
     session = create_session()
+
+    # используем обработчик ошибок
     try:
+        # создаем объект
         post_orm = PostORM(
             text=data['text'],
             image=data['image'],
             author_username=data['author_username'],
         )
+
+        # добавляем объект
         session.add(post_orm)
+
+        # сохраняем в БД
         session.commit()
+
+        # преобразовываем в json
         resp = jsonify({'id': post_orm.id, 'location': f'/posts/{post_orm.id}'})
+
+        # ставим status code created
         resp.status_code = 201
+
+        # возвращаем
         return resp
+
+    # при ошибке
     except Exception as e:
+        # откатываем изменения
         session.rollback()
+
+        # возвращаем ошибку
         return jsonify({'error': 'INTERNAL', 'detail': str(e)}), 500
+
+    # в самом конце
     finally:
+        #закрываем сессию
         session.close()
